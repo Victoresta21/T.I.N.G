cc2d9cadc6230c7f454bc798c0b541f2e9e8aaf1
Victor Emmanuel <victorpassos2012@gmail.com>
2022-09-20 20:20:51 -0300

    Requisito 3 feito

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return
+    
+    text_reader = txt_importer(path_file)
+
+    process_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text_reader),
+        "linhas_do_arquivo": text_reader
+    }
+    
+    instance.enqueue(process_file)
+    print(process_file, file=sys.stdout)
 
 
 def remove(instance):
