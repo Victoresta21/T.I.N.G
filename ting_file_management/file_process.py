6af13ed3641b473cdef03d157d20a3057aed9726
Victor Emmanuel <victorpassos2012@gmail.com>
2022-09-20 20:21:52 -0300

    FIX Lint do req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -6,7 +6,7 @@ def process(path_file, instance):
     for i in range(len(instance)):
         if instance.search(i)["nome_do_arquivo"] == path_file:
             return
-    
+
     text_reader = txt_importer(path_file)
 
     process_file = {
@@ -14,7 +14,7 @@ def process(path_file, instance):
         "qtd_linhas": len(text_reader),
         "linhas_do_arquivo": text_reader
     }
-    
+
     instance.enqueue(process_file)
     print(process_file, file=sys.stdout)
 
