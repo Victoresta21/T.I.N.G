d2b9a636d44f6345f036c19c712bd771419e159d
Victor Emmanuel <victorpassos2012@gmail.com>
2022-09-20 23:22:45 -0300

    Requisito 6 feito

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files_with_the_words = []
+    word_ocorrencies = []
+
+    for file in range(len(instance.data)):
+        for index, line in enumerate(
+            instance.search(file)["linhas_do_arquivo"]
+        ):
+            if word.lower() in line.lower():
+                word_ocorrencies.append({"linha": index + 1})
+
+        if len(word_ocorrencies) != 0:
+            files_with_the_words.append({
+                "palavra": word,
+                "arquivo": instance.search(file)["nome_do_arquivo"],
+                "ocorrencias": word_ocorrencies,
+            })
+
+    return files_with_the_words
 
 
 def search_by_word(word, instance):
